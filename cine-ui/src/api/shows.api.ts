fcd090641fa37cdb8cced5a8955914ef7a5b61b9
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-02-11 21:28:17 -0500

    simulacro

---
+++ cine-ui/src/api/shows.api.ts
@@ -0,0 +1,29 @@
+import { http } from "./http";
+    
+export type Paginated<T> = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+};
+
+export type Show = { id: number; movie_title: string, room: string, price: number, available_seat: number };
+
+export async function listShowsApi() {
+  const { data } = await http.get<Paginated<Show>>("/api/shows/");
+  return data; // { count, next, previous, results }
+}
+
+export async function createShowApi(movie_title: string, room: string, price: number, available_seat: number) {
+  const { data } = await http.post<Show>("/api/shows/", { movie_title, room, price, available_seat });
+  return data;
+}
+
+export async function updateShowApi(id: number, movie_title: string) {
+  const { data } = await http.put<Show>(`/api/shows/${id}/`, { movie_title });
+  return data;
+}
+
+export async function deleteShowApi(id: number) {
+  await http.delete(`/api/shows/${id}/`);
+}
